2b8463a8fcda9d788620faa706fb837dcededd2c
Kaio G <kaiogreco91@gmail.com>
2022-03-07 14:28:57 -0300

    iniciando o projeto

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
 
 class App extends React.Component {
   render() {
-    return (<p>TrybeTunes</p>);
+    return (
+      <BrowserRouter>
+        <main>
+          <Route path="/search" component={ Search } />
+          <Route path="/album/:id" component={ Album } />
+          <Route path="/favorites" component={ Favorites } />
+          <Route path="/profile" component={ Profile } />
+          <Route path="/profile/edit" component={ ProfileEdit } />
+        </main>
+      </BrowserRouter>
+    );
   }
 }
 
