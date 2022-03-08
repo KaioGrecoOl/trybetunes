dd6de48e500c32f1ead4bdec8eb922d7d9056ca4
Kaio G <kaiogreco91@gmail.com>
2022-03-08 18:13:21 -0300

    req.2 - criando formulario identificação

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,10 +1,80 @@
+import { Redirect } from 'react-router-dom';
 import React, { Component } from 'react';
+import Loading from '../Component/Loading';
+import { createUser } from '../services/userAPI';
 
 class Login extends Component {
+  constructor() {
+    super();
+    this.state = {
+      isLoginButtonDisabled: true,
+      loginInput: '',
+      loading: false,
+      redirect: false,
+    };
+
+    this.onInputChange = this.onInputChange.bind(this);
+    this.validationButton = this.validationButton.bind(this);
+  }
+
+  onInputChange({ target }) {
+    const { name, value } = target;
+    this.setState({ [name]: value },
+      () => this.validationButton());
+  }
+
+  async buttonSave() {
+    const { loginInput } = this.state;
+    this.setState({ loading: true });
+    await createUser({ name: loginInput });
+    this.setState({ loading: false,
+      redirect: true,
+    });
+  }
+
+  validationButton() {
+    const minimLetter = 3;
+    const { loginInput } = this.state;
+
+    if (loginInput.length >= minimLetter) {
+      this.setState({ isLoginButtonDisabled: false });
+    } else {
+      this.setState({ isLoginButtonDisabled: true });
+    }
+  }
+
   render() {
+    const { loginInput,
+      isLoginButtonDisabled, loading, redirect } = this.state;
+
     return (
       <div data-testid="page-login">
-        <h2>Estou na página de login</h2>
+        {loading
+          ? (<Loading />)
+          : (
+            <form>
+              <label htmlFor="loginInput">
+                Login:
+                <input
+                  type="text"
+                  name="loginInput"
+                  value={ loginInput }
+                  data-testid="login-name-input"
+                  onChange={ this.onInputChange }
+                  placeholder="Name"
+                />
+              </label>
+              <button
+                type="button"
+                disabled={ isLoginButtonDisabled }
+                data-testid="login-submit-button"
+                onClick={ () => this.buttonSave() }
+              >
+                Entrar
+              </button>
+              { redirect && <Redirect to="/search" /> }
+            </form>
+          )}
       </div>
     );
   }
